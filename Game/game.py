612524f4dbf7df7c80a6c028436fcab2c2661540
Shiza <shizashaikh456@gmail.com>
2024-05-20 02:19:47 -0400

    added player movement across the xy plane for now

--- Game/game.py
+++ Game/game.py
@@ -7,14 +7,35 @@ import math
 # button4 = drawRectangle(screen, "#0c0182", 495, 100, 130, 40, "8 minutes", "white", 18)
 # button5 = drawRectangle(screen, "#0c0182", 655, 100, 130, 40, "Quit", "white", 18)
 
+class Player:
+    def __init__(self, x, y, width, height, colour, score, screen_width, screen_height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.colour = colour
+        self.visible = True
+        self.score = 0
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+    def draw(self, screen):
+        if self.visible:
+            pygame.draw.rect(screen, pygame.Color(self.colour), self.rect)
+
+    def move(self, dx):
+        new_x = self.rect.x + dx
+        if 0 <= new_x <= self.screen_width - self.rect.width:
+            self.rect.x = new_x
+            
+    def getPlayerScore(self, score):
+        return self.score
+
 
 
 class soccerSlimeGame:
-    def __init__(self, player1Score, player2Score):
-        self.player1Score = 0
-        self.player2Score = 0
-        self.x = 0
-        self.y = 0
+    def __init__(self):
+        self.width = 830
+        self.height = 400
+        self.player1 = Player(0, 325, 80, 5, "white", 0, self.width, self.height)
+        self.player2 = Player(720, 325, 80, 5, "white", 0, self.width, self.height)
         self.visible = True
         
     @staticmethod
@@ -41,10 +62,12 @@ class soccerSlimeGame:
     def getP2Score(self, player2Score):
         return self.player2Score
     
+    
         
     def renderGraphics(self):
         # Game loop starts here
         pygame.init()
+        clock = pygame.time.Clock()
         screen = pygame.display.set_mode((800, 430))  
         pygame.display.set_caption("Soccer Slime")
         font = pygame.font.Font("freesansbold.ttf", 18)
@@ -53,29 +76,37 @@ class soccerSlimeGame:
         
         running = True
         while running:
+            clock.tick(60)
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                     pygame.quit()
                     
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        player1.x -= 10
-                    elif event.key == pygame.K_RIGHT:
-                        player1.x += 10
+                keys = pygame.key.get_pressed()
+                    
+            if keys[pygame.K_LEFT]:    
+                self.player1.move(-5)
+                        
+            if keys[pygame.K_RIGHT]:
+               self.player1.move(5)
+               
+            if keys[pygame.K_d]:
+                self.player2.move(5)
+                
+            if keys[pygame.K_a]:
+                self.player2.move(-5)
                     
                 # if you need it, call visibility function using self.toggle_visibility() AFTER any sort of event handler
                 
-                screen.fill("blue")
-                rect = self.createRect(screen, "#808080", 0, (height - 100), 800, 100, "slime soccer!!!!1!1!!!!!!!!!", "gray", 16, self.visible)
+            screen.fill("blue")
+            self.createRect(screen, "#808080", 0, (height - 100), 800, 100, "slime soccer!!!!1!1!!!!!!!!!", "gray", 16, self.visible)
                 
-                player1 = self.createRect(screen, "white", 0, (height - 110), 80, 10, "", "white", 1, self.visible)
+            self.player1.draw(screen)
+            self.player2.draw(screen)
 
-                center = (width // 2, height // 2)
-                #pygame.draw.arc(surface, color, rect, start_angle, stop_angle, width=1)
-                
-                pygame.display.update()
+            pygame.display.update()
 
 if __name__ == "__main__":
-    game = soccerSlimeGame(0, 0)
+    game = soccerSlimeGame()
     game.renderGraphics()
